113d81bfed6cb120d927d8177a116ab5cf4bcd44
Calvin Herman <calvinherman@CalvinMacBook.local>
2019-09-24 11:45:18 -0400

    add one more code

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+"""
+This code is printing hello world
+Calvin Herman
+Sep 3, 2019
+"""
+
+print('hello world!')
+
+"""
+for i in range(10):
+    print('hello world0')
+print('hello world1')
+#while True:
+#    print('hello world0')
+"""
